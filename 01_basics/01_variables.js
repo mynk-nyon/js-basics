34822805b78911342bdca68d95d70465e9f26542
Mayank Shukla <77397565+mynk-nyon@users.noreply.github.com>
2024-12-21 10:11:35 +0000

    variables done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,25 @@
+const accountId = 144553
+let accountEmail = "mayankshukla.acc@gmail.com"
+var accountPassword = "12345"
+accountCity = "Punjab"
+let accountState;
+
+// accountId = 2   Not allowed
+accountEmail = "mynk2308@gmail.com"
+accountPassword = "1847569238"
+accountCity = "Lakhimpur kheri"
+
+console.log(accountId);
+console.log(accountEmail);
+console.log(accountPassword);
+console.log(accountCity);
+
+//One more method
+
+/*
+Prefer not to use var 
+because of issue in block scope and functional scope
+*/
+
+console.table
+([accountId, accountEmail, accountPassword, accountCity, accountState])
