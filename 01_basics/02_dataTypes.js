6e5793c524306943c80094b474ae49fd7f868f92
Mayank Shukla <77397565+mynk-nyon@users.noreply.github.com>
2024-12-21 10:10:49 +0000

    datatypes done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,34 @@
+"use strict"; //treat all JS code as newer version
+
+//alert("Hello")  we are using nodejs not browser
+
+
+
+console.log(3+3); 
+//Readability should be high
+console.log("Mayank")
+
+/*
+                      Important Documentations
+                       -  ec39
+                       -  MDN
+*/
+
+
+
+// Data Types
+
+let name = "mayank"
+let age = 21
+let isLoggedIn = true
+
+// range of number ==>> 2 to power 53 
+// bigint
+// string => " " or ''
+// boolean => true/false
+// null => standalone value
+// undefined => variable is declared but value is not given
+// symbol => when we talk about unique things
+//object
+
+console.log(typeof undefined);
