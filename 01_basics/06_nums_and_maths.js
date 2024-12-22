6f1075028b2fb0d559aad1200bda183b5df15fe1
Mayank Shukla <77397565+mynk-nyon@users.noreply.github.com>
2024-12-22 18:05:58 +0000

    add file nums and maths

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,23 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(200)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+const otherNumber = 23.8966
+
+// console.log(otherNumber.toPrecision(3));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+//+++++++++++++++++++Maths+++++++++++++++
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.6));
+
