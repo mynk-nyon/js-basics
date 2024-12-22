b8dbe93b37e7eff2bf10da4a59a9884e711d6403
Mayank Shukla <77397565+mynk-nyon@users.noreply.github.com>
2024-12-22 18:24:24 +0000

    dates in js

---
+++ 01_basics/07_dates_in_js.js
@@ -0,0 +1,13 @@
+// DATES
+
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+// let myCreatedDate = new Date(2024, 11, 23)
+// let myCreatedDate = new Date(2024, 11, 23, 23, 53)
+
+
+console.log(myCreatedDate.toLocaleString());
