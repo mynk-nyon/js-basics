8d97f5ec96734bce86f439ef99c7bdee06e4c218
Mayank Shukla <77397565+mynk-nyon@users.noreply.github.com>
2024-12-24 18:17:35 +0000

    created array in basics_02

---
+++ 02_basics/01_array.js
@@ -0,0 +1,36 @@
+// array
+
+const myArr = [0, 1, 2, 3, 4, 5]
+
+// const myHeros = ["Mayank", "Thor"]
+
+// const myNewHeros = new Array("Yash", "Aman", "Raj")
+// console.log(myNewHeros);
+// myNewHeros.push("Jay", "Sahil")
+// myNewHeros.pop()
+
+// myArr.unshift(20) add values at first
+
+// myArr.shift() Removes value at first
+
+//console.log(myArr.includes(10));  Boolean Type check if element is in the array or not 
+
+// console.log(myArr.indexOf(4)); Tells at which index the specific value is
+
+// console.log(myArr);
+
+// Slice,  Splice
+
+console.log("A", myArr);
+const myn1 = myArr.slice(1, 3)
+console.log(myn1);
+
+console.log("B", myArr);
+
+const myn2 = myArr.splice(1,3)
+console.log(myn2);
+console.log("C", myArr);
+
+
+
+
