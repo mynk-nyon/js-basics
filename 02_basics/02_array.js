306317dcc0827dc36d3fbd34d1ede4a44dcfe1dd
Mayank Shukla <77397565+mynk-nyon@users.noreply.github.com>
2024-12-24 18:27:32 +0000

    created 02_array in 02_basics

---
+++ 02_basics/02_array.js
@@ -0,0 +1,13 @@
+const marvel_heros = ["thor", "ironman", "spiderman"]
+
+const dc_heros = ["flash", "superman", "batman"]
+
+// marvel_heros.push(dc_heros)
+console.log(marvel_heros);
+// console.log(marvel_heros[3][1]);
+
+const new_superheros = marvel_heros.concat(dc_heros)
+console.log(new_superheros);
+
+
+
