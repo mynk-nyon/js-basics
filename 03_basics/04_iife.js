5df9b049c15a057e2d41435ddf4d3e388cd1a90b
Mayank Shukla <77397565+mynk-nyon@users.noreply.github.com>
2024-12-26 18:46:04 +0000

    created IIFe file in 03_basics

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,10 @@
+// Immediately Involked Function Expressions (IIFE)
+
+(function chai(){
+    //named IIFE
+    console.log('DB CONNECTED');
+})();
+
+( (name) => {
+    console.log(`DB CONNECTED TWO ${name}`)
+}) ("Mayank")
