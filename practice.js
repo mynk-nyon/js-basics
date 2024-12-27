16b89f859a8ae2241a039d68c5fcd8c79c7dcd9c
Mayank Shukla <77397565+mynk-nyon@users.noreply.github.com>
2024-12-27 17:23:30 +0000

    update this to check progress

---
+++ practice.js
@@ -0,0 +1,13 @@
+let name
+const schoolName = "Lovely Professional University"
+let classRoom
+
+name = "mayank"
+classRoom = "MCA"
+
+// console.table([name, schoolName, classRoom]);
+
+console.log(typeof null);
+
+
+
