eb05ec73f10c83cf6ca3d5d7cd04bd7f1f7b8e74
Mayank Shukla <77397565+mynk-nyon@users.noreply.github.com>
2024-12-29 18:40:39 +0000

    changes is six.js

--- 05_iterations/six.js
+++ 05_iterations/six.js
@@ -6,12 +6,28 @@ const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 //     return num > 4
 // })
 
-const newNums = []
+// const newNums = []
 
-myNums.forEach( (num) => {
-    if(num > 4){
-        newNums.push(num)
+// myNums.forEach( (num) => {
+//     if(num > 4){
+//         newNums.push(num)
+//     }
+// } )
+
+// console.log(newNums);
+
+const books = [
+    {title: 'Book1', genre: 'Fiction', publish: 1981, edition: 2004},
+    {title: 'Book2', genre: 'Non-Fiction', publish: 1992, edition: 2008},
+    {title: 'Book3', genre: 'History', publish: 2003, edition: 2007},
+    {title: 'Book4', genre: 'Science', publish: 2007, edition: 2010}
+]
+
+// const userBooks = books.filter( (bk) => bk.genre === 'History')
+
+const userBooks = books.filter
+((bk) => {
+        return bk.publish >= 2000 &&bk.genre == 'History'
     }
-} )
-
-console.log(newNums);
+)
+console.log(userBooks);
