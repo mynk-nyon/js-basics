d790e8d13cad00d454d974ccc74df3bafc25f6e4
Mayank Shukla <77397565+mynk-nyon@users.noreply.github.com>
2024-12-29 12:08:57 +0000

    created twojs in 05_iterations

---
+++ 05_iterations/two.js
@@ -0,0 +1,24 @@
+// while and do while
+
+// let index = 0
+// while (index <=10) {
+//     console.log(`Value of index is ${index}`);
+//     index = index + 2
+// }
+
+// let myArray = ["Superman", "Batman", "Ironman", "Flash"]
+
+// let arr = 0
+// while (arr < myArray.length) {
+//     console.log(`Value of ${myArray[arr]}`);
+//     arr = arr + 1
+// }
+
+
+// do while (do iteration one time then check condition)
+
+let score = 11
+do {
+    console.log(`score is ${score}`);
+    score++
+} while (score <= 10);
